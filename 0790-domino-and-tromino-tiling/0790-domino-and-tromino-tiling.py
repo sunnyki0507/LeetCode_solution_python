607e6ef4495afb4351e4941a77086e2d580b8338
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-05 16:31:04 -0800

    Time: 2 ms (55.48%), Space: 12.6 MB (32.88%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -13,18 +13,6 @@ class Solution(object):
         for i in range(4, n+1):
             dp[i] = dp[i-1] * 2 +dp[i-3] 
         return dp[n] % mod
-        # if n == 1:
-        #     return 1
-
-        # def sol(num, gap):
-        #     if num > n:
-        #         return 0
-        #     if num == n:
-        #         return not gap
-        #     if gap:
-        #         return sol(num+1,False) + sol(num+1,True)
-        #     return sol(num+1, False) + sol(num+2, False)+2*sol(num+2,True)
-        # return sol(0, False)
 
         """
         :type n: int
