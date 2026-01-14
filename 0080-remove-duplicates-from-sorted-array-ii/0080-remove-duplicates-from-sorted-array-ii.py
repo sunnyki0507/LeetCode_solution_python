093eba4627192eca5869db5b48d5211653dd24ef
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-14 11:03:17 -0800

    Time: 70 ms (57.58%), Space: 15.5 MB (20.24%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,20 +1,28 @@
 class Solution(object):
     def removeDuplicates(self, nums):
-        erase = []
-        prev = nums[0]
-        repeat = 1
-        for i in range(1, len(nums)):
-            if prev == nums[i]:
-                repeat += 1
-            else:
-                repeat = 1
-            prev = nums[i]
-            if repeat > 2:
-                erase.append(nums[i])
-        while erase:
-            idx = nums.index(erase.pop())
-            nums.pop(idx)
+        idx = 0
+        for i in nums:
+            if (idx == 0) or (idx == 1) or (nums[idx-2] != i):
+                nums[idx] = i
+                idx += 1
+        del nums[idx:]
+
         return len(nums)
+        # erase = []
+        # prev = nums[0]
+        # repeat = 1
+        # for i in range(1, len(nums)):
+        #     if prev == nums[i]:
+        #         repeat += 1
+        #     else:
+        #         repeat = 1
+        #     prev = nums[i]
+        #     if repeat > 2:
+        #         erase.append(nums[i])
+        # while erase:
+        #     idx = nums.index(erase.pop())
+        #     nums.pop(idx)
+        # return len(nums)
 
 
         """
