ea0a29235220ef843f80fa6d9357301c21e7a063
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-14 14:30:48 -0800

    Time: 5 ms (76.85%), Space: 13.7 MB (33.66%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,20 +1,33 @@
 class Solution(object):
     def majorityElement(self, nums):
-        dic = {}
+        count = 0
+        maxN = nums[0]
         for i in nums:
-            if i not in dic:
-                dic[i] = 0
-            dic[i] += 1
-        print(dic)
-        maxN = 0
-        idx = -1
-        for k,v in dic.items():
-            if v > maxN:
-                maxN = v
-                idx = k
-                print(maxN)
-                print(idx)
-        return idx
+            if count == 0:
+                maxN = i
+                count += 1
+            elif maxN == i:
+                count += 1
+            else:
+                count -= 1
+        return maxN
+            
+
+        # dic = {}
+        # for i in nums:
+        #     if i not in dic:
+        #         dic[i] = 0
+        #     dic[i] += 1
+        # print(dic)
+        # maxN = 0
+        # idx = -1
+        # for k,v in dic.items():
+        #     if v > maxN:
+        #         maxN = v
+        #         idx = k
+        #         print(maxN)
+        #         print(idx)
+        # return idx
 
         """
         :type nums: List[int]
