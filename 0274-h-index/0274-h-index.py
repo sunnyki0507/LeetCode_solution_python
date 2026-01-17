0fcdf2486f84100757ab447e87b3cae0295f8899
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-16 23:40:15 -0800

    Time: 70 ms (8.77%), Space: 12.8 MB (39.78%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def hIndex(self, citations):
+        arr = list(citations)
+        papers = []
+        h = 0
+        while arr:
+            num = max(arr)
+            idx = arr.index(num)
+            papers.append(arr.pop(idx))
+            # print(num)
+            # print(papers)
+            if len(papers) <= num:
+                h = max(h,len(papers))
+                # print(h)
+                # print("Hi")
+            else:
+                h = max(h, num)
+                # print(h)
+                # print("Bye")
+                break
+        return h
+
+
+
+        """
+        :type citations: List[int]
+        :rtype: int
+        """
+        
