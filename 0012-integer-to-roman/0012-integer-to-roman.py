54c612a9e8c788db600e2267c8866121d70eec8f
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-19 14:20:13 -0800

    Time: 15 ms (16.78%), Space: 12.4 MB (54.75%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,40 +1,55 @@
 class Solution(object):
     def intToRoman(self, num):
-        roman = {}
-        roman[1] = 'I'
-        roman[4] = 'IV'
-        roman[5] = 'V'
-        roman[9] = 'IX'
-        roman[10] = 'X'
-        roman[40] = 'XL'
-        roman[50] = 'L'
-        roman[90] = 'XC'
-        roman[100] = 'C'
-        roman[400] = 'CD'
-        roman[500] = 'D'
-        roman[900] = 'CM'
-        roman[1000] = 'M'
-        string = ""
+        roman = [(1000,'M'), (900,'CM'), (500,'D'), (400,'CD'), (100,'C'),
+        (90,'XC'),(50,'L'),(40,'XL'),(10, 'X'),(9,'IX'),(5,'V'),(4,'IV'),
+        (1,'I')]
 
-        strNum = str(num)
-        length = len(strNum)
-        for i in range(length):
-            n = int(strNum[i])
-            unit = 10 ** (length-1-i)
-            if n < 5:
-                if n == 4:
-                    string += roman[n*unit]
-                    continue
-                string += (roman[unit] * n)
-                continue
-            if n == 9:
-                string += roman[n*unit]
-                continue
-            string += roman[unit*5]
-            n = n-5
-            string += (roman[unit] * n)
+        out = []
 
-        return string
+        for v,c in roman:
+            count = num // v
+            num = num - (count*v)
+            out.append(c*count)
+            if num == 0:
+                break
+        return "".join(out)
+
+
+        # roman = {}
+        # roman[1] = 'I'
+        # roman[4] = 'IV'
+        # roman[5] = 'V'
+        # roman[9] = 'IX'
+        # roman[10] = 'X'
+        # roman[40] = 'XL'
+        # roman[50] = 'L'
+        # roman[90] = 'XC'
+        # roman[100] = 'C'
+        # roman[400] = 'CD'
+        # roman[500] = 'D'
+        # roman[900] = 'CM'
+        # roman[1000] = 'M'
+        # string = ""
+
+        # strNum = str(num)
+        # length = len(strNum)
+        # for i in range(length):
+        #     n = int(strNum[i])
+        #     unit = 10 ** (length-1-i)
+        #     if n < 5:
+        #         if n == 4:
+        #             string += roman[n*unit]
+        #             continue
+        #         string += (roman[unit] * n)
+        #         continue
+        #     if n == 9:
+        #         string += roman[n*unit]
+        #         continue
+        #     string += roman[unit*5]
+        #     n = n-5
+        #     string += (roman[unit] * n)
+
+        # return string
 
         """
         :type num: int
