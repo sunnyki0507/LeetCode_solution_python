65bde0e30efbe8f07042193c32ad8474b4d7213e
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-19 15:17:06 -0800

    Time: 0 ms (100%), Space: 12.6 MB (48.23%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.py
+++ 0058-length-of-last-word/0058-length-of-last-word.py
@@ -1,15 +1,28 @@
 class Solution(object):
     def lengthOfLastWord(self, s):
-        rev = "".join(reversed(s))
+
         length = 0
-        for i in rev:
-            if i == ' ' and length == 0:
-                continue
-            if i == ' ':
-                return length
+        adding = True
+        for i in s:
+            if i != ' ':
+                if not adding:
+                    adding  = True
+                    length = 1
+                else:
+                    length += 1
             else:
-                length += 1
+                adding = False
         return length
+        # rev = "".join(reversed(s))
+        # length = 0
+        # for i in rev:
+        #     if i == ' ' and length == 0:
+        #         continue
+        #     if i == ' ':
+        #         return length
+        #     else:
+        #         length += 1
+        # return length
         """
         :type s: str
         :rtype: int
