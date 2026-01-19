46509ddd51bdf26062eba5fa0256375d443df5f2
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-19 14:28:11 -0800

    Time: 1 ms (91.29%), Space: 12.6 MB (17.24%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.py
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -1,18 +1,24 @@
 class Solution(object):
     def intToRoman(self, num):
-        roman = [(1000,'M'), (900,'CM'), (500,'D'), (400,'CD'), (100,'C'),
-        (90,'XC'),(50,'L'),(40,'XL'),(10, 'X'),(9,'IX'),(5,'V'),(4,'IV'),
-        (1,'I')]
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num//1000]+C[(num%1000)//100]+X[(num%100)//10]+I[(num%10)]
 
-        out = []
+        # roman = [(1000,'M'), (900,'CM'), (500,'D'), (400,'CD'), (100,'C'),
+        # (90,'XC'),(50,'L'),(40,'XL'),(10, 'X'),(9,'IX'),(5,'V'),(4,'IV'),
+        # (1,'I')]
 
-        for v,c in roman:
-            count = num // v
-            num = num - (count*v)
-            out.append(c*count)
-            if num == 0:
-                break
-        return "".join(out)
+        # out = []
+
+        # for v,c in roman:
+        #     count = num // v
+        #     num = num - (count*v)
+        #     out.append(c*count)
+        #     if num == 0:
+        #         break
+        # return "".join(out)
 
 
         # roman = {}
