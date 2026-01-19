fe36617910161fef0cff8a7e3ecb58411dd8e22f
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-18 20:09:09 -0800

    Time: 205 ms (5.1%), Space: 14.4 MB (8.52%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def trap(self, height):
+        length = len(height)
+        water = [0] * length
+        h = height[0]
+        for i in range(1, length):
+            if height[i] > h:
+                h = height[i]
+                continue
+            water[i] = h - height[i]
+        h = height[length-1]
+        water[length-1] = 0
+        for i in range(length-2,-1,-1):
+            if height[i] > h:
+                water[i] = 0
+                h = height[i]
+                continue
+            if water[i] > (h - height[i]):
+                water[i] = h - height[i]
+        print(water)
+        return sum(water)
+
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        
