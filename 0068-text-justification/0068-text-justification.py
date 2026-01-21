82822ec64885ba4059cc2f74eeb44bdd99e509fa
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-21 02:14:19 -0800

    Time: 0 ms (100%), Space: 12.5 MB (21.41%) - LeetHub

--- 0068-text-justification/0068-text-justification.py
+++ 0068-text-justification/0068-text-justification.py
@@ -1,20 +1,77 @@
-class Solution:
-    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
         res = []
-        
-        cur_wlist  = []
-        cur_ch_cnt = 0   # for the space consideration
-     
-        for w in words: 
-            if cur_ch_cnt + len(cur_wlist) + len(w) > maxWidth:
-                for i in range(maxWidth - cur_ch_cnt):  # insert space
-                    cur_wlist[i%(len(cur_wlist) - 1 or 1)] += ' '
-               
-                res.append(''.join(cur_wlist)) # insert a this row 
-                cur_wlist, cur_ch_cnt = [], 0 # prepare a new row, resetting
-                
-            cur_wlist += [w]
-            cur_ch_cnt += len(w)
-            
-        return res + [' '.join(cur_wlist).ljust(maxWidth)]
+        n = len(words)
+        i = 0
+        while i<n:
+            j = i
+            length = 0
+            while j<n and length + len(words[j]) + j - i <= maxWidth:
+                length += len(words[j])
+                j += 1
+            gap = maxWidth - length
+            count = j-i-1
+            add = ''
+            if j == n or count == 0:
+                for w in range(i, j):
+                    if w != (j-1):
+                        add += words[w] + ' '
+                    else:
+                        add += words[w] + ' '*(gap - count)
+                res.append(add)
+            else:
+                each = gap//count
+                extra = gap % count
+                for w in range(i,j):
+                    if w != (j-1):
+                        if extra > 0:
+                            add += words[w] + ' ' * (each+1)
+                        else:
+                            add += words[w] + ' ' * each
+                    else:
+                        add += words[w]
+                    extra -= 1
+                res.append(add)
+            i = j
+        return res
+
+
+
+
+        # res = []
+        # add = []
+        # string = ""
+        # for i in words:
+        #     if len(string) == 0:
+        #         string += i
+        #         add.append(i)
+        #         continue
+        #     else:
+        #         if (len(string) + len(i) + 1) <= maxWidth:
+        #             string += " "
+        #             string += i
+        #             add.append(i)
+        #         else:
+        #             left = maxWidth - len(string)
+        #             gap = len(add) - 1
+        #             if gap == 0:
+        #                 gap = 1
+        #             for j in range(left):
+        #                 add[j%gap] += " "
+        #             res.append(" ".join(add))
+        #             string = i
+        #             add = []
+        #             add.append(i)
+
+        # if add:
+        #     left = maxWidth - len(string)
+        #     for j in range(left):
+        #         string += " "
+        #     res.append(string)
+        # return res
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
         
