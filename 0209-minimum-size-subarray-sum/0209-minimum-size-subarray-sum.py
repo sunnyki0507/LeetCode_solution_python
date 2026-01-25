c8328a5f25ea699b585cdd6f7b1d2431166a2965
sunnyki0507 <150889313+sunnyki0507@users.noreply.github.com>
2026-01-25 00:22:51 -0800

    Time: 14 ms (56.96%), Space: 20 MB (62.25%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,31 +1,52 @@
 class Solution(object):
     def minSubArrayLen(self, target, nums):
+
+
         n = len(nums)
-        minN = n+1
-        curr = []
-        s = nums[0]
-        i = 1
-        curr.append(nums[0])
+        minN = float("inf")
+        right = 0
+        left = 0
+        s = 0
+
+        for i in range(n):
+            s += nums[i]
+            right = i
+            
+            while s >= target:
+                if right - left + 1 < minN:
+                    minN = right - left + 1
+                s -= nums[left]
+                left += 1
+
+        return minN if minN != float("inf") else 0
+
+
+        # n = len(nums)
+        # minN = n+1
+        # curr = []
+        # s = nums[0]
+        # i = 1
+        # curr.append(nums[0])
         
-        while i < n:
-            print(s)
-            if s >= target:
-                if len(curr) < minN:
-                    minN = len(curr)
-                s -= curr.pop(0)
-            else:
-                curr.append(nums[i])
-                s += nums[i]
-                i += 1
-        while curr and s >= target:
-            print(s)
-            if s >= target:
-                if len(curr) < minN:
-                    minN = len(curr)
-                s -= curr.pop(0)
-        if minN == n+1:
-            return 0
-        return minN
+        # while i < n:
+        #     print(s)
+        #     if s >= target:
+        #         if len(curr) < minN:
+        #             minN = len(curr)
+        #         s -= curr.pop(0)
+        #     else:
+        #         curr.append(nums[i])
+        #         s += nums[i]
+        #         i += 1
+        # while curr and s >= target:
+        #     print(s)
+        #     if s >= target:
+        #         if len(curr) < minN:
+        #             minN = len(curr)
+        #         s -= curr.pop(0)
+        # if minN == n+1:
+        #     return 0
+        # return minN
 
 
 
